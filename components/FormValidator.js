ed0b4ec6e266066577240006cfa3705cdcc6615e
DennyH <Dennyh_20@icloud.com>
2026-04-02 14:37:02 -0400

    updates for sprint 7

---
+++ components/FormValidator.js
@@ -0,0 +1,52 @@
+class FormValidator {
+  constructor(settings, formEl) {
+    this._inputSelector = settings.inputSelector;
+    this._formSelector = settings.formSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._errorClass = settings.errorClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._formEl = formEl;
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      showInputError(
+        this._formEl,
+        inputElement,
+        inputElement.validationMessage,
+        this._settings,
+      );
+    } else {
+      hideInputError(this._formEl, inputElement, settings);
+    }
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector),
+    );
+    const buttonElement = formElement.querySelector(
+      settings.submitButtonSelector,
+    );
+
+    toggleButtonState(inputList, buttonElement, settings);
+
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        checkInputValidity(formElement, inputElement, settings);
+        toggleButtonState(inputList, buttonElement, settings);
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+
+    this._setEventListeners();
+  }
+}
+
+export default FormValidator;
